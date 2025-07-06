e82cb1c41e8c42d5aaf60ac806a6cad521780a5d
Jatin <jatinsathe696@gmail.com>
2025-07-06 13:59:13 +0530

    Fix MIME type issue

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'  // Make sure this line exists
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>,
+  
 )
